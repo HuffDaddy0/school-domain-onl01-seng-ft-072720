39e88c8f2082d6aca79df96355d23fa8848dbe0b
Tyler Huffman <ty.huff24@gmail.com>
2020-08-03 21:30:55 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade]||=[]
+    @roster[grade] << name
+  end
+
+
+def grade
+  @roster[grade]
+end
+
+def sort
+  @roster[grade].map {|students|
+    students.sort {|one , two|
+    one <=> two}}
+@roster.flatten
+end
+end
